cbf57597680c80e781ed6cdcc2ed1beff6dffdfd
EdivanMayolo <edivanmayolo98@gmail.com>
2025-05-14 21:31:35 -0300

    Commit

---
+++ Service/DTOs/EnderecoDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace ApiClientes.Service.DTOs
+{
+   
+        public class CriarEnderecoDTO
+        {
+            public string cep { get; set; }
+            public string logradouro { get; set; }
+            public string numero { get; set; }
+            public string bairro { get; set; }
+            public string cidade { get; set; }
+            public string uf { get; set; }
+            public string status { get; set; }
+
+        }
+        public class EnderecoDTO
+        {
+        public int Id { get; set; }
+        public int cep { get; set; }
+        public string logradouro { get; set; }
+        public string numero { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string cidade { get; set; }
+        public string uf { get; set; }
+        public int clienteid { get; set; }
+        public int status { get; set; }
+    }
+
+}
---
+++ Service/Parses/EnderecoParse.cs
@@ -0,0 +1,36 @@
+﻿using ApiClientes.Database.Models;
+using ApiClientes.Service.DTOs;
+
+namespace ApiClientes.Service.Parses
+{
+    public class EnderecoParse
+    {
+        public static TbEndereco ToTbEndereco(CriarEnderecoDTO dto)
+        {
+            TbEndereco novoEndereco = new();
+            novoEndereco.Cep = int.Parse(dto.cep);
+            novoEndereco.Logradouro = dto.logradouro;
+            novoEndereco.Numero = dto.numero;
+            novoEndereco.Bairro = dto.bairro;
+            novoEndereco.Cidade = dto.cidade;
+            novoEndereco.Uf = dto.uf;
+            novoEndereco.Status = int.Parse(dto.status);
+            return novoEndereco;
+        }
+        public static EnderecoDTO ToEnderecoDTO(TbEndereco tbEndereco)
+        {
+            EnderecoDTO Response = new();
+            Response.Id = tbEndereco.Id;
+            Response.logradouro = tbEndereco.Logradouro;
+            Response.numero = tbEndereco.Numero;
+            Response.bairro = tbEndereco.Bairro;
+            Response.cidade = tbEndereco.Cidade;
+            Response.uf = tbEndereco.Uf;
+            Response.cep = tbEndereco.Cep;
+            Response.clienteid = tbEndereco.Clienteid;
+            Response.status = tbEndereco.Status;
+
+            return Response;
+        }
+    }
+}
